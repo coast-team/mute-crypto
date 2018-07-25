8a145fb4ee8f1659bdb38001a5e07b5a462de7e6
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-07-25 10:28:34 +0200

    chore(dist): add dist to .gitignore

--- dist/commonjs/KeyStatusEnum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var KeyStatusEnum;
-(function (KeyStatusEnum) {
-    KeyStatusEnum[KeyStatusEnum["UNDEFINED"] = 0] = "UNDEFINED";
-    KeyStatusEnum[KeyStatusEnum["CALCUL_IN_PROGRESS"] = 1] = "CALCUL_IN_PROGRESS";
-    KeyStatusEnum[KeyStatusEnum["READY"] = 2] = "READY";
-})(KeyStatusEnum = exports.KeyStatusEnum || (exports.KeyStatusEnum = {}));
-//# sourceMappingURL=KeyStatusEnum.js.map
